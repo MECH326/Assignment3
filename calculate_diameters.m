4bb901f30f9e5e3139ff8df04d7e91b5d63f6cf4
cekaps <cekaps@gmail.com>
2019-11-05 18:02:10 -0800

    hi

--- calculate_diameters.m
+++ calculate_diameters.m
@@ -12,27 +12,28 @@ x = (0.1125*0.225*pi*(d2^2)/4 + 0.325*0.3875*pi*(d1^2)/4 + 0.15*0.625*pi*(d2^2)/
     (0.225*pi*(d2^2)/4 + 0.325*pi*(d1^2)/4 + 0.15*pi*(d2^2)/4);
 
 %% Determine radii of gears
-r_H = 660/6600;
-r_S = 660/3300;
+r_H = 660/6600
+
+r_S = 660/3300
 
 %% Determine reaction forces at A and B
 Ax = 0; % A is not locating bearing
 
 % Find Ay: Balance z-moments about B
-Ay = ((1200+w_S)*0.150 + (6600+w_H)*0.625)/0.850;
+Ay = ((1200+w_S)*0.150 + (6600+w_H)*0.625)/0.70;
 
 % Find Az: Balance y-moments about B
-Az = (3300*0.150 + 2400*0.625)/0.850;
+Az = (3300*0.150 + 2400*0.625)/0.70;
 
 A = [Ax Ay Az];
 
 Bx = -900;  % B is locating bearing
 
 % Find By: Balance z-moments about A
-By = ((1200+w_S)*0.700 + (6600+w_H)*0.225)/0.850;
+By = ((1200+w_S)*0.700 + (6600+w_H)*0.225)/0.70;
 
 % Find Bz: Balance y-moments about A
-Bz = (3300*0.700 + 2400*0.225)/0.850;
+Bz = (3300*0.700 + 2400*0.225)/0.70;
 
 B = [Bx By Bz];
 
